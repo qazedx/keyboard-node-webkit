504120da3d25b5da52ce48dc75ab2752e42afe16
Mindaugas Sasnauskas <mindaugas.s@wilibox.com>
2015-08-10 16:55:46 +0300

    angular in progress 0.0

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var gui = require('nw.gui');
+// var angular = require('angular');
+// var express = require('express');
 
 navigator.webkitGetUserMedia({video: true},
   function(stream) {
     document.getElementById('camera').src = URL.createObjectURL(stream);
+
   },
   function() {
     alert('could not connect stream');
